41d3ac9615f04c9215aa9372748fd13da526ba1a
Adame <Adame.haouari@gmail.com>
2021-01-11 09:11:56 +0100

    fin exo

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+let fruits = ['bananes', 'pommes', 'cerises', 'oranges', 'tomates' ];
+
+fruits.forEach(element => {
+    console.log(`fruit: ${element}`); 
+});
+
+console.log('----');
+
+
+
+let coding16 = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"]; 
+
+coding16.forEach((element, i ) => {
+    //console.log(`element no: ${coding16.indexOf(element)} Bonjour ${element} `); 
+    console.log(`numero => ${i} Bonjour ${element}`);
+});
+
+
+
